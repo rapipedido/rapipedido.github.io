5d1df732ec010a4be4e916b09cb5d10319453a62
rapipedido <64939398+rapipedido@users.noreply.github.com>
2020-05-10 19:00:07 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1ZYNr6cqcAfShPwmXK1mnf3mcgv2i64-TfgxpDJH_zvs/edit?usp=sharing';
+
+function init() {
+    Tabletop.init( { key: publicSpreadsheetUrl,
+                     callback: showInfo,
+                     simpleSheet: true } )
+}
+
+function showInfo(data, tabletop) {
+    console.log(data);
+    var parsed = "";			
+    let lista = document.getElementById('lista');
+    var i=0;
+  	$.each( data, function( y, item ) {
+  		parsed += "<div class='item'><div class='div-item-img'>";
+  		if(item["Unidades en stock"]>0){
+  			parsed += "<img class='item-img'";
+        parsed +=" src='"+item.Imagen+"'></div>"; 
+        parsed +="<div class='item-desc'><h3 class='desc'>"+item.Marca+" "+item.Titulo+"</h3>"; 
+        parsed +="<p>"+item.Descripcion+"</p>"; 
+        parsed +="<input type='text' class='price' value='"+item["Precio Bs F"].substring(2,item["Precio Bs F"].length-3).replace(",",'.').replace(",",'.')+" Bs.F' disabled='true'></div>"; 
+        parsed +="<div class='item-qtd'><input type='button' class='btn' id='plus' value='-' onclick='process(-1,"+i+", "+item["Unidades en stock"]+")' />"; 
+        parsed +="<input name='quant' class='quant' size='1' type='text' value='0' disabled='True' />"; 
+        parsed +="<input type='button' class='btn' id='minus' value='+' onclick='process(1,"+i+", "+item["Unidades en stock"]+")'><br>"; 
+        parsed +="</div></div>";  
+  		}else{
+  			parsed += "<img class='item-img-out'";
+        parsed +=" src='"+item.Imagen+"' width='88' height='88'></div>"; 
+        parsed +="<div class='item-desc'><h3 class='desc' style='color: #555'>"+item.Marca+" "+item.Titulo+"</h3>"; 
+        parsed +="<p style='color: #555'>"+item.Descripcion+"</p>"; 
+        parsed +="<input type='text' class='price-out' value='"+item["Precio Bs F"].substring(2,item["Precio Bs F"].length-3).replace(",",'.').replace(",",'.')+" Bs.F' disabled='true'></div>"; 
+        parsed +="<div class='item-qtd'><input type='button' class='btn' value='-' onclick='process(-1,"+i+", "+item["Unidades en stock"]+")'  disabled='True'/>"; 
+        parsed +="<input name='quant' class='quant-out' size='1' type='text' value='0' disabled='True' />"; 
+        parsed +="<input type='button' class='btn' value='+' onclick='process(1,"+i+", "+item["Unidades en stock"]+")' disabled='True'><br>"; 
+        parsed +="</div></div>";  
+  		}        
+  			i++;
+  		}
+    );                       
+    document.getElementById('lista').innerHTML = parsed;
+}
+
+function process(quant, i, max){
+    var val = parseInt
+(document.getElementsByClassName("quant")[i].value);
+    val += quant;
+    if(val < 0){
+      document.getElementsByClassName("quant")[i].value = 0;
+    }else if(val > max){
+      document.getElementsByClassName("quant")[i].value = max;
+    }else{
+    	document.getElementsByClassName("quant")[i].value = val;
+    }
+    var t = 
+    document.getElementById("total").value = 0;
+    for(var y=0 ; y<document.getElementsByClassName("quant").length;y++){
+    	document.getElementById("total").value = (parseInt(document.getElementById("total").value) + 
+    		parseInt(document.getElementsByClassName("quant")[y].value.replace(".",'').replace(".",'')) * 
+    		parseInt(document.getElementsByClassName("price")[y].value.replace(".",'').replace(".",'').substring(0,(document.getElementsByClassName("price")[y].value.length-3))));
+    }
+    document.getElementById("total").value = parseInt(document.getElementById("total").value).toLocaleString("pt") + " Bs.F" ;
+    msg();
+}
+
+function msg(){
+	var d = new Date();
+	var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+	var msg = "https://api.whatsapp.com/send?phone=+584147660652&text=*Pedido* - Fecha " + d.getDate() + "%20" + months[d.getMonth()] + "%20" + d.getFullYear();
+	for(var y=0 ; y<document.getElementsByClassName("quant").length;y++){
+		if(parseInt(document.getElementsByClassName("quant")[y].value)>0){
+    		msg += "%0A"+ document.getElementsByClassName("quant")[y].value + "x " + document.getElementsByClassName("desc")[y].textContent;
+    	}
+	}
+    msg += "%0A%0A" + "Total: " + document.getElementById("total").value;
+    msg += "%0A%0A" + "Tu pedido no esta confirmado,%0Aespera una respuesta para la confirmacion."
+    document.getElementById("btn_img").href = msg;
+}
+
+
+window.addEventListener('DOMContentLoaded', init)
