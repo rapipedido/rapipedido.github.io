6cd9cd06db33bc3af662b04f48fc27cb5b72f0b5
rapipedido <64939398+rapipedido@users.noreply.github.com>
2020-05-22 17:37:43 -0700

    refactoring

--- script.js
+++ script.js
@@ -16,6 +16,22 @@ function init() {
     })
 }
 
+function validProduct(item) {
+  if (item["Precio Bs F"] != "" &&
+      item.Marca != "" &&
+      item.Imagen != "" &&
+      item.Titulo != "" &&
+      item.Descripcion != "" &&
+      item["Unidades en stock"] != "" &&
+      item["Unidades en stock"] != "#VALUE!" &&
+      item["Precio Bs F"] != "#VALUE!") {
+        return true;
+      }
+      else {
+        return false; 
+      }
+}
+
 function showInfo(data, tabletop) {
     console.log(data.data);
     $( ".spinner" ).remove();
@@ -32,37 +48,36 @@ function showInfo(data, tabletop) {
       $.trim(item.Marca = item.Marca);
       $.trim(item.Titulo = item.Titulo);
       $.trim(item.Descripcion = item.Descripcion);
-      if(item["Precio Bs F"] != "" && item.Marca != "" && item.Imagen != "" && item.Titulo != "" && item.Descripcion != "" && item["Unidades en stock"] != "" &&  item["Unidades en stock"] != "#VALUE!"&&  item["Precio Bs F"] != "#VALUE!"){
-      	stock = item["Unidades en stock"];
-        //precio = item["Precio Bs F"].substring(2,item["Precio Bs F"].length-3).replace(",",'.').replace(",",'.');
-        precio = VEF(item["Precio Bs F"]).format();
-        precio_usd = USD(item["Precio USD"]).format();
-        if($.isNumeric(parseInt(stock))){
-        //if(isNumberDot(precio) && $.isNumeric(parseInt(stock))){
-          parsed += "<div class='item'><div class='div-item-img'>";
-      		if(item["Unidades en stock"] > 0){
-      			parsed += "<img class='item-img'";
-            parsed +=" src='"+item.Imagen+"'></div>"; 
-            parsed +="<div class='item-desc'><h3 class='desc'>"+item.Marca+" "+item.Titulo+"</h3>"; 
-            parsed +="<p>"+item.Descripcion+"</p>"; 
-            parsed +="<input type='text' class='price' value='"+precio+" Bs.' disabled='True'><span class='price-usd'> | $"+precio_usd+"</span></div>"; 
-            parsed +="<div class='item-qtd'><input type='button' class='btn' id='plus' value='-' onclick='process(-1,"+i+", "+stock+")' />"; 
-            parsed +="<input name='quant' class='quant' size='1' type='text' value='0' disabled='True' />"; 
-            parsed +="<input type='button' class='btn' id='minus' value='+' onclick='process(1,"+i+", "+stock+")'><br>"; 
-            parsed +="</div></div>";  
-      		}else{
-      			parsed += "<img class='item-img-out'";
-            parsed +=" src='"+item.Imagen+"' width='88' height='88'></div>"; 
-            parsed +="<div class='item-desc'><h3 class='desc' style='color: #555'>"+item.Marca+" "+item.Titulo+"</h3>"; 
-            parsed +="<p style='color: #555'>"+item.Descripcion+"</p>"; 
-            parsed +="<input type='text' class='price' style='color: #555;-webkit-text-fill-color: #555;' value='"+precio+" Bs.' disabled='true'></div>"; 
-            parsed +="<div class='item-qtd'><input type='button' class='btn' value='-' onclick='process(-1,"+i+", "+stock+")'  disabled='True'/>"; 
-            parsed +="<input name='quant' class='quant' style='color: #555;-webkit-text-fill-color: #555;' size='1' type='text' value='0' disabled='True' />"; 
-            parsed +="<input type='button' class='btn' value='+' onclick='process(1,"+i+", "+stock+")' disabled='True'><br>"; 
-            parsed +="</div></div>";  
-      		}        
-      		i++;
-        }
+      if(validProduct(item)) {
+          stock = item["Unidades en stock"];
+          precio = VEF(item["Precio Bs F"]).format();
+          precio_usd = USD(item["Precio USD"]).format();
+          if($.isNumeric(parseInt(stock))){
+          //if(isNumberDot(precio) && $.isNumeric(parseInt(stock))){
+            parsed += "<div class='item'><div class='div-item-img'>";
+            if(item["Unidades en stock"] > 0){
+              parsed += "<img class='item-img'";
+              parsed +=" src='"+item.Imagen+"'></div>"; 
+              parsed +="<div class='item-desc'><h3 class='desc'>"+item.Marca+" "+item.Titulo+"</h3>"; 
+              parsed +="<p>"+item.Descripcion+"</p>"; 
+              parsed +="<input type='text' class='price' value='"+precio+" Bs.' disabled='True'><span class='price-usd'> | $"+precio_usd+"</span></div>"; 
+              parsed +="<div class='item-qtd'><input type='button' class='btn' id='plus' value='-' onclick='process(-1,"+i+", "+stock+")' />"; 
+              parsed +="<input name='quant' class='quant' size='1' type='text' value='0' disabled='True' />"; 
+              parsed +="<input type='button' class='btn' id='minus' value='+' onclick='process(1,"+i+", "+stock+")'><br>"; 
+              parsed +="</div></div>";  
+            }else{ // OOS Items
+              parsed += "<img class='item-img-out'";
+              parsed +=" src='"+item.Imagen+"' width='88' height='88'></div>"; 
+              parsed +="<div class='item-desc'><h3 class='desc' style='color: #555'>"+item.Marca+" "+item.Titulo+"</h3>"; 
+              parsed +="<p style='color: #555'>"+item.Descripcion+"</p>"; 
+              parsed +="<input type='text' class='price' style='color: #555;-webkit-text-fill-color: #555;' value='"+precio+" Bs.' disabled='true'></div>"; 
+              parsed +="<div class='item-qtd'><input type='button' class='btn' value='-' onclick='process(-1,"+i+", "+stock+")'  disabled='True'/>"; 
+              parsed +="<input name='quant' class='quant' style='color: #555;-webkit-text-fill-color: #555;' size='1' type='text' value='0' disabled='True' />"; 
+              parsed +="<input type='button' class='btn' value='+' onclick='process(1,"+i+", "+stock+")' disabled='True'><br>"; 
+              parsed +="</div></div>";  
+            }        
+            i++;
+          }
       }
   	});                       
     document.getElementById('lista').innerHTML = parsed;
@@ -80,8 +95,9 @@ function process(quant, i, max){
     }
     var t = document.getElementById("total").value = 0;
     for(var y=0 ; y<document.getElementsByClassName("quant").length;y++){
-    	document.getElementById("total").value = (parseInt(document.getElementById("total").value) + 
-    		parseInt(document.getElementsByClassName("quant")[y].value.replace(".",'').replace(".",'')) * 
+      document.getElementById("total").value = 
+        VEF(document.getElementById("total").value) + 
+        (document.getElementsByClassName("quant")[y].value * 
     		VEF(document.getElementsByClassName("price")[y].value));
     }
     // Add price to nav bar
@@ -106,15 +122,6 @@ function msg(){
 
   // Add new text to message
   document.getElementById("btn_img").href = base_url + encodeURIComponent(msg);
-}
-
-function isNumberDot(valor){
-  for(var x = 0; valor.length > x ; x++){
-    if (!($.isNumeric(parseInt(valor[x])) || (valor[x] == '.') && x != 0)){
-      return false;
-    }
-  }
-  return true;
 }
 
 window.addEventListener('DOMContentLoaded', init)
